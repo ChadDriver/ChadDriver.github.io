af90e9b39e5799a4359d8bf2808e4ef817377c94
ChadDriver <vyvanze70@proton.me>
2025-02-12 18:12:01 -0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Replace 'Drive' with your GitHub repository name
 export default defineConfig({
   plugins: [react()],
+  base: '/Drive/', // This is required for GitHub Pages deployment
   server: {
     host: '0.0.0.0',
   }
-})
+});

390c70f02e0e86344faf10580dabf0376b01f046
ChadDriver <vyvanze70@proton.me>
2025-02-12 19:39:43 -0500

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/Drive/',
+  base: 'ChadDriver/Drive/',
   build: {
     outDir: 'dist',
   },
